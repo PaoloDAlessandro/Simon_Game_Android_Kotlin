fd0fa00c8e8b980f764b5bf78abd1478e15ce060
Paolo Dalex <paolodalex96@gmail.com>
2022-12-06 21:44:56 +0100

    feat: translated java code to kotlin, error on user click timeout

--- app/src/main/java/it/itsar/simon_game_android_kotlin/MainActivity.kt
+++ app/src/main/java/it/itsar/simon_game_android_kotlin/MainActivity.kt
@@ -7,11 +7,20 @@ import androidx.core.graphics.ColorUtils
 import it.itsar.simon_game_android_kotlin.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
+private const val alphaDelay: Int = 500
+private const val alphaValue: Double = 0.5
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var colori: Array<Colore>
+    private var userSequence: ArrayList<String> = ArrayList()
+    private var sequence: ArrayList<String> = ArrayList()
+
+    private var score: Int = 0
+    private var indice: Int = 0
+    private var nClicks: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,27 +35,74 @@ class MainActivity : AppCompatActivity() {
 
 
         setButtonsBackgroundColor()
-        addClickListeners();
-        
+        addClickListeners()
+        disableButtons()
+
         binding.playButton.setOnClickListener {
-            colori[0].play()
+            reset()
+            updateScore()
+            enableButtons()
+            play()
         }
     }
 
     fun setButtonsBackgroundColor() {
         for(colore in colori) {
-            println(colore.button.toString())
-            colore.button.alpha = (0.5).toFloat()
+            colore.button.alpha = (alphaValue).toFloat()
+        }
+    }
+
+    fun disableButtons() {
+        for(colore in colori) {
+            colore.button.isEnabled = false
+        }
+    }
+
+    fun enableButtons() {
+        for(colore in colori) {
+            colore.button.isEnabled = true
         }
     }
 
     fun addClickListeners() {
-        for(colore in colori) {
+        colori.forEachIndexed { index, colore ->
+            val finalI: Int = index
             colore.button.setOnClickListener {
+                userSequence.add(colori[index].nome)
+                nClicks++
+                if(!(userSequence.equals(sequence.subList(0, nClicks)))) {
+                    gameOver()
+                }
+                if(userSequence.equals(sequence) && sequence.size > 0) {
+                    win()
+                }
                 colore.button.alpha = 1.toFloat()
-                setTimeOut({colore.button.alpha = 0.5.toFloat()}, 500)
+                colori[finalI].play()
+                setTimeOut({ this.runOnUiThread{ colore.button.alpha = alphaValue.toFloat() }}, alphaDelay)
+                var finalnClicks = nClicks
+                setTimeOut({checkClick(finalnClicks)}, 1500)
             }
         }
+    }
+
+    fun gameOver() {
+        this.runOnUiThread {
+            binding.score.text = "Game over"
+            binding.playButton.isEnabled = true
+        }
+        reset()
+    }
+
+    fun win() {
+        nClicks = 0
+        indice = 0
+        score++
+        updateScore()
+        setTimeOut({play()}, 500)
+    }
+
+    fun updateScore() {
+        this.binding.score.text = "Your score: " + score
     }
 
     fun setTimeOut(runnable: Runnable, delay: Int) {
@@ -56,4 +112,47 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun play() {
+        userSequence.clear()
+        val index: Int = (Math.random() * colori.size).toInt()
+        sequence.add(colori[index].nome)
+        setTimeOut({playSound(colori[findIndex(sequence[indice])])}, 500)
+    }
+
+    fun findIndex(nome: String): Int {
+        colori.forEachIndexed { index, colore ->
+            if(colore.nome.equals(nome)) {
+                return index
+            }
+        }
+        return 0
+    }
+
+    fun reset() {
+        indice = 0
+        nClicks = 0
+        score = 0
+        sequence.clear()
+        userSequence.clear()
+    }
+
+    fun checkClick(preClick: Int) {
+        if(nClicks < preClick + 1 && nClicks != 0) {
+            gameOver()
+        }
+    }
+
+    fun changeButtonAlpha(color: Colore) {
+        color.button.alpha = 1.toFloat()
+        setTimeOut({this.runOnUiThread({color.button.alpha = alphaValue.toFloat()})}, alphaDelay)
+    }
+
+    fun playSound(color: Colore) {
+        color.play()
+        changeButtonAlpha(color)
+        indice++
+        if(indice < sequence.size) {
+            setTimeOut({playSound(color)}, alphaDelay)
+        }
+    }
 }
