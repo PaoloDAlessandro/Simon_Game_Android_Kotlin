c203b199ec1f9ad11f2ddd953e9f9f2814e619f6
Paolo Dalex <paolodalex96@gmail.com>
2022-12-08 00:54:16 +0100

    fix: now user cannot click buttons while computer sequence is playing

--- app/src/main/java/it/itsar/simon_game_android_kotlin/MainActivity.kt
+++ app/src/main/java/it/itsar/simon_game_android_kotlin/MainActivity.kt
@@ -2,8 +2,9 @@ package it.itsar.simon_game_android_kotlin
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.ColorUtils
 import it.itsar.simon_game_android_kotlin.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
@@ -13,6 +14,7 @@ private const val alphaValue: Double = 0.5
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var playButton: Button
 
     private lateinit var colori: Array<Colore>
     private var userSequence: ArrayList<String> = ArrayList()
@@ -27,73 +29,77 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        playButton = binding.playButton
         colori = arrayOf(
             Colore("red", 329, 250, binding.redButton, Color.RED),
             Colore("green", 391, 250, binding.greenButton, Color.GREEN),
             Colore("blue", 195, 250, binding.blueButton, Color.BLUE),
             Colore("yellow", 261, 250, binding.yellowButton, Color.YELLOW))
 
-
         setButtonsBackgroundColor()
         addClickListeners()
         disableButtons()
 
-        binding.playButton.setOnClickListener {
+        playButton.setOnClickListener {
             reset()
             updateScore()
             enableButtons()
+            playButton.isEnabled = false
             play()
         }
     }
 
-    fun setButtonsBackgroundColor() {
+    private fun setButtonsBackgroundColor() {
         for(colore in colori) {
             colore.button.alpha = (alphaValue).toFloat()
         }
     }
 
-    fun disableButtons() {
+    private fun disableButtons() {
         for(colore in colori) {
             colore.button.isEnabled = false
         }
     }
 
-    fun enableButtons() {
+    private fun enableButtons() {
         for(colore in colori) {
             colore.button.isEnabled = true
         }
     }
 
-    fun addClickListeners() {
+    private fun addClickListeners() {
         colori.forEachIndexed { index, colore ->
             val finalI: Int = index
             colore.button.setOnClickListener {
                 userSequence.add(colori[index].nome)
                 nClicks++
-                if(!(userSequence.equals(sequence.subList(0, nClicks)))) {
+                Log.d("userSequence: ", userSequence.toString())
+                Log.d("sequence: ", sequence.toString())
+                if(userSequence != sequence.subList(0, nClicks)) {
                     gameOver()
                 }
-                if(userSequence.equals(sequence) && sequence.size > 0) {
+                if(userSequence == sequence && sequence.size > 0) {
                     win()
                 }
                 colore.button.alpha = 1.toFloat()
                 colori[finalI].play()
                 setTimeOut({ this.runOnUiThread{ colore.button.alpha = alphaValue.toFloat() }}, alphaDelay)
-                var finalnClicks = nClicks
+                val finalnClicks = nClicks
                 setTimeOut({checkClick(finalnClicks)}, 1500)
             }
         }
     }
 
-    fun gameOver() {
+    private fun gameOver() {
         this.runOnUiThread {
+            disableButtons()
             binding.score.text = "Game over"
-            binding.playButton.isEnabled = true
+            playButton.isEnabled = true
         }
         reset()
     }
 
-    fun win() {
+    private fun win() {
         nClicks = 0
         indice = 0
         score++
@@ -101,34 +107,35 @@ class MainActivity : AppCompatActivity() {
         setTimeOut({play()}, 500)
     }
 
-    fun updateScore() {
+    private fun updateScore() {
         this.binding.score.text = "Your score: " + score
     }
 
-    fun setTimeOut(runnable: Runnable, delay: Int) {
+    private fun setTimeOut(runnable: Runnable, delay: Int) {
         thread(start = true) {
             Thread.sleep(delay.toLong())
             runnable.run()
         }
     }
 
-    fun play() {
+    private fun play() {
+        this.runOnUiThread(this::enableButtons)
         userSequence.clear()
         val index: Int = (Math.random() * colori.size).toInt()
         sequence.add(colori[index].nome)
         setTimeOut({playSound(colori[findIndex(sequence[indice])])}, 500)
     }
 
-    fun findIndex(nome: String): Int {
+    private fun findIndex(nome: String): Int {
         colori.forEachIndexed { index, colore ->
-            if(colore.nome.equals(nome)) {
+            if(colore.nome == nome) {
                 return index
             }
         }
         return 0
     }
 
-    fun reset() {
+    private fun reset() {
         indice = 0
         nClicks = 0
         score = 0
@@ -136,23 +143,26 @@ class MainActivity : AppCompatActivity() {
         userSequence.clear()
     }
 
-    fun checkClick(preClick: Int) {
+    private fun checkClick(preClick: Int) {
         if(nClicks < preClick + 1 && nClicks != 0) {
             gameOver()
         }
     }
 
-    fun changeButtonAlpha(color: Colore) {
+    private fun changeButtonAlpha(color: Colore) {
         color.button.alpha = 1.toFloat()
-        setTimeOut({this.runOnUiThread({color.button.alpha = alphaValue.toFloat()})}, alphaDelay)
+        setTimeOut({this.runOnUiThread { color.button.alpha = alphaValue.toFloat() } }, alphaDelay)
     }
 
-    fun playSound(color: Colore) {
+    private fun playSound(color: Colore) {
         color.play()
         changeButtonAlpha(color)
         indice++
         if(indice < sequence.size) {
-            setTimeOut({playSound(color)}, alphaDelay)
+            setTimeOut({playSound(colori[findIndex(sequence[indice])])}, alphaDelay)
+        }
+        else {
+            this.runOnUiThread(this::enableButtons)
         }
     }
 }
